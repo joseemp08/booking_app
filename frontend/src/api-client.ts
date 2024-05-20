6c996add4bc916a054575af9b47effde480febb8
jose <jose.moreno2099@gmail.com>
2024-05-20 15:46:51 -0700

    toast-notification

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -6,15 +6,14 @@ export const register = async (formData: RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
         method: 'POST',
         headers: {
-            "Content-Type":"application/json"
+            "Content-Type":"application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
     });
 
     const responseBody = await response.json();
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error(responseBody.message);
-        
-    }
-}
+      }
+};
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from '../api-client'; 
 import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
     fistName: string,
@@ -12,6 +13,7 @@ export type RegisterFormData = {
 }
 
 const Register = () => {
+    const navigate = useNavigate();
 
     const{ showToast }= useAppContext();
 
@@ -22,11 +24,12 @@ const Register = () => {
     const mutation = useMutation(apiClient.register, 
         {onSuccess: () => {
             //console.log("registration succesful!!")
-            showToast({ message: "Registration success!!", type:"SUCCESS" })
+            showToast({ message: "Registration success!!", type: "SUCCESS" });
+            navigate("/")
         },
         onError: (error: Error) => {
             //console.log(error.message);
-            showToast({ message:error.message, type: "ERROR" })
+            showToast({ message:error.message, type: "ERROR" });
         }
       });
 
