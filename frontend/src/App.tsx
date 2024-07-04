d147432129c7d4251d0e0ce7abd207fca371846f
jose <jose.moreno2099@gmail.com>
2024-07-03 23:47:53 -0700

    booking, stripe payment and home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,23 +13,31 @@ import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
+import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 const App = () => {
-  const {isLoggedIn} = useAppContext();
-  return(
+  const { isLoggedIn } = useAppContext();
+  return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <Layout>
-            <p>Home Page</p>
-          </Layout>
-        } />
-
-        <Route path="/search" element={
-          <Layout>
-            <Search />
-          </Layout>
-        } />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <Search />
+            </Layout>
+          }
+        />
         <Route
           path="/detail/:hotelId"
           element={
@@ -38,50 +46,72 @@ const App = () => {
             </Layout>
           }
         />
-        <Route path="/register" element={
-          <Layout>
-            <Register />
-          </Layout>
-        }/>
-        <Route 
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route
           path="/sign-in"
-          element= {
+          element={
             <Layout>
               <SignIn />
             </Layout>
           }
         />
 
-        {isLoggedIn && <>
-          <Route 
-            path="/add-hotel"
-            element={
-              <Layout>
-                <AddHotel />
-              </Layout>
-            }
-          />
-          <Route
-            path="/my-hotels"
-            element={
-              <Layout>
-                <MyHotels />
-              </Layout>
-            }
-          />
-          <Route
-            path="/edit-hotel/:hotelId"
-            element={
-              <Layout>
-                <EditHotel />
-              </Layout>
-            }
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/hotel/:hotelId/booking"
+              element={
+                <Layout>
+                  <Booking />
+                </Layout>
+              }
             />
-        </>}
+
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
+                </Layout>
+              }
+            />
+          </>
+        )}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
